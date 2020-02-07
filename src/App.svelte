51103f2fe0c110b33dcd0492693eb24d9b597dd4
DCCA <dcca12@gmail.com>
2020-02-06 16:17:31 -0800

    add app to github

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+  import Profile from "./Profile.svelte";
+  import Todos from "./Todos.svelte";
+
+  import { auth, googleProvider } from "./firebase";
+  import { authState } from "rxfire/auth";
+
+  let user;
+
+  const unsubscribe = authState(auth).subscribe(u => {
+	console.log(u);
+	  user = u;
+  });
+
+  function login() {
+    auth.signInWithPopup(googleProvider);
+  }
+</script>
+
+<style>
+	section{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+</style>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.8.0/css/bulma.min.css">
+</svelte:head>
+
+<section>
+  {#if user}
+    <Profile {...user} />
+    <button class="button is-primary" on:click={() => auth.signOut()}>Logout</button>
+    <hr />
+    <Todos uid={user.uid} />
+  {:else}
+    <button class="button is-primary" on:click={login}>Sign-in with Google</button>
+  {/if}
+</section>
---
+++ src/TodoItem.svelte
@@ -0,0 +1,64 @@
+<script>
+
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+    
+    function remove() {
+		dispatch('remove', { id });
+	}
+
+	function toggleStatus() {
+        let newStatus = !complete;
+		dispatch('toggle', {
+            id,
+            newStatus
+        });
+    }
+    
+
+    export let id; // document ID
+    export let text;
+    export let complete;
+</script>
+
+<style>
+    .is-complete {
+        text-decoration: line-through;
+        color: green;
+    }
+    li{
+      border: solid;
+      border-radius: 2rem;
+      padding: 1rem;
+      margin: 0.2rem 0;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+    button{
+      border: none;
+      background-color: white;
+    }
+    .end{
+      align-self: flex-end;
+    }
+    .items {
+      padding-right: 3rem;
+    }
+</style>
+
+
+<li>
+
+{#if complete}
+    <span class="items is-complete">{ text }</span>
+    <button class="end" on:click={toggleStatus}> ✔️ </button>
+{:else}
+    <span class="items">{ text }</span>
+    <button class="end" on:click={toggleStatus}> ❌ </button>
+{/if}
+
+<button on:click={remove}> 🗑 </button>
+
+</li>
